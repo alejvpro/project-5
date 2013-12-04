bd564c6be2f72775ce1d074f75cd982a23791674
Jay Singh <jayveer@ucla.edu>
2013-12-03 21:55:07 -0800

    payservlet

--- src/edu/ucla/cs/cs144/PayServlet.java
+++ src/edu/ucla/cs/cs144/PayServlet.java
@@ -15,8 +15,89 @@ public class PayServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-		request.getRequestDispatcher("/pay.jsp").forward(request, response);
 		
+        String itemId = request.getParameter("id");
+
+        // basically it first gets
+
+        HttpSession session = request.getSession(false);
+
+        // check if session variable is null
+
+        if (session == null)
+        {
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<body>");
+            out.println("<h1>No results found! Please try searching again</h1>");
+            
+            out.println("<p>Enter an item ID below to see it's info</p>");
+
+            out.println("<form action=\"item\" method=\"GET\">");
+            out.println("Item Id: <input type=\"text\" name=\"id\"><br>");
+            out.println("<input type=\"submit\" />");
+            out.println("</form>");
+            
+            out.println("</body>");
+            out.println("</html>"); 
+        }
+        else
+        {
+            HashMap<String, String> itemPrices = (HashMap<String, String)session.getAttribute("itemPrices");
+
+            if (itemPrices == null)
+            {
+                PrintWriter out = response.getWriter();
+                out.println("<html>");
+                out.println("<body>");
+                out.println("<h1>No results found! Please try searching again</h1>");
+                
+                out.println("<p>Enter an item ID below to see it's info</p>");
+
+                out.println("<form action=\"item\" method=\"GET\">");
+                out.println("Item Id: <input type=\"text\" name=\"id\"><br>");
+                out.println("<input type=\"submit\" />");
+                out.println("</form>");
+                
+                out.println("</body>");
+                out.println("</html>"); 
+            }
+            else if(itemPrices.contains(itemId))
+            {
+                // Display normally using itemPRices Price
+
+                request.setAttribute("link", "https://" + request.getServerName() + "/" + request.getContextPath());
+                request.getRequestDispatcher("/pay.jsp").forward(request, response);
+            }
+            else
+            {
+                PrintWriter out = response.getWriter();
+                out.println("<html>");
+                out.println("<body>");
+                out.println("<h1>No results found! Please try searching again</h1>");
+                
+                out.println("<p>Enter an item ID below to see it's info</p>");
+
+                out.println("<form action=\"item\" method=\"GET\">");
+                out.println("Item Id: <input type=\"text\" name=\"id\"><br>");
+                out.println("<input type=\"submit\" />");
+                out.println("</form>");
+                
+                out.println("</body>");
+                out.println("</html>"); 
+
+            }
+
+
+
+        }
+
+
+        // else
+
+
+
+
     	/*String itemId = request.getParameter("id");
     	
     	String itemXML = AuctionSearchClient.getXMLDataForItemId(itemId);
