7027d0356421deb2b8b8bf9ec7f6f55c49b33bcd
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-12-03 19:50:11 -0800

    Added session initiation to ItemServlet

--- src/edu/ucla/cs/cs144/ItemServlet.java
+++ src/edu/ucla/cs/cs144/ItemServlet.java
@@ -3,11 +3,14 @@ package edu.ucla.cs.cs144;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.util.HashMap;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ItemServlet extends HttpServlet implements Servlet {
        
@@ -20,6 +23,32 @@ public class ItemServlet extends HttpServlet implements Servlet {
     	String itemXML = AuctionSearchClient.getXMLDataForItemId(itemId);
     	if(itemXML != null && !itemXML.equals(""))
     	{
+    		// Create an item based on the XML
+    		ItemBean item = BeanParser.processItemXMLString(itemXML);
+    		
+    		// Check if this item can be purchased immediately
+    		String buyPrice = item.getBuyPrice();
+    		if(buyPrice != null && buyPrice != "")
+    		{
+    			// Get the previous session or create a new one
+    			HttpSession session = request.getSession(true);
+    			
+    			// Check if session already has previous items
+    			HashMap<String, String> itemPrices = (HashMap<String, String>)session.getAttribute("itemPrices");
+    			if (itemPrices == null) 
+    			{
+    				// Create a new session if neccessary
+    				itemPrices = new HashMap<String, String>();
+    			}
+    			
+    			// Add this item's price
+				itemPrices.put(item.getItemId(), item.getBuyPrice());
+				
+				// Set or reset the session's item HashMap
+				session.setAttribute("itemPrices", itemPrices);
+    		}
+    		
+    		// Set the current item attribute and forward the request to the view
     		request.setAttribute("item", BeanParser.processItemXMLString(itemXML));
     		request.getRequestDispatcher("/item.jsp").forward(request, response);
     	}
